f42ff42e65b7166b623f7d7ec9aea5d4a2c36729
Gautam KJ <97722291+GautamKJ@users.noreply.github.com>
2022-09-01 12:33:44 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.58%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(vector<int>&nums,int house,int n, vector<int>&dp){
+        if(house>=n)
+            return 0;
+        if(dp[house]!=-1)
+            return dp[house];
+        int inc=INT_MIN,exc=INT_MIN;
+         inc=nums[house]+solve(nums,house+2,n,dp);
+        exc=solve(nums,house+1,n,dp);
+        
+        return dp[house]= max(inc,exc);
+    }
+    int rob(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        return max(solve(nums,0,n,dp),solve(nums,1,n,dp));
+    }
+};
