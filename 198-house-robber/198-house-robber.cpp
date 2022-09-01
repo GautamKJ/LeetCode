d1b7c34995385efa781fc19113271fbb3e988216
Gautam KJ <97722291+GautamKJ@users.noreply.github.com>
2022-09-01 12:34:09 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.58%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -14,7 +14,7 @@ public:
     int rob(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n,-1);
         return solve(nums,0,n,dp);
     }
 };
