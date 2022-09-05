f14c167fa179bade735c0bc9a0839acaf9a726f6
Gautam KJ <97722291+GautamKJ@users.noreply.github.com>
2022-09-05 13:27:04 +0530

    Time: 4 ms (41.28%), Space: 5.7 MB (88.99%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -3,16 +3,19 @@ public:
     bool isPowerOfTwo(int n) {
         
         int cnt=0,zero=0;
-//         for(int i=0;i<31;i++){
+        for(int i=0;i<31;i++){
             
-//             if(((n>>i)&1)==1)
-//                 cnt++;
-//             else
-//                 zero++;
-//         }
-//         cout<<cnt<<endl;
+            if(((n>>i)&1)==1)
+                cnt++;
+            else
+                zero++;
+        }
+        // cout<<cnt<<endl;
 //         cout<<zero<<endl;
-//         return cnt!=1?false:true;
+        if(n>0 && cnt==1)
+            return true;
+        return false;
+        return cnt!=1 ?false:true;
         
        return n > 0 && not (n & n - 1);
        
