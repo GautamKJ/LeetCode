e53f91849d3a5c4f20435bd2215e3e002fa1a5b4
Gautam KJ <97722291+GautamKJ@users.noreply.github.com>
2022-09-07 12:21:00 +0530

    Time: 16 ms (60.55%), Space: 7.7 MB (46.42%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        
+        int c2=0,c3=0,c5=0;
+        
+        vector<int>dp(n+1);
+        dp[0]=1;
+        
+        for(int i=1;i<=n;i++){
+            dp[i]=min({2*dp[c2],3*dp[c3],5*dp[c5]});
+            
+            if(dp[i]==2*dp[c2])
+                c2++;
+             if(dp[i]==3*dp[c3])
+                c3++;
+             if(dp[i]==5*dp[c5])
+                c5++;
+        }
+        return dp[n-1];
+    }
+};
