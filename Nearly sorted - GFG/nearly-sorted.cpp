7a248519b43c05ec481974ee628594f4077edd23
Gautam KJ <97722291+GautamKJ@users.noreply.github.com>
2022-12-23 14:40:16 +0530

    Added solution - LeetHub

--- Nearly sorted - GFG/nearly-sorted.cpp
+++ Nearly sorted - GFG/nearly-sorted.cpp
@@ -11,11 +11,26 @@ class Solution
     vector <int> nearlySorted(int arr[], int num, int K){
         vector<int>res;
         
-        sort(arr,arr+num);
-        for(int i=0;i<num;i++){
-            res.push_back(arr[i]);
-        }
-        return res;
+       priority_queue <int, vector<int>, greater<int> > pq;
+       
+       for(int i=0;i<=K;i++){
+           pq.push(arr[i]);
+       }
+       
+       for(int i=K+1;i<num;i++){
+           
+           int ele=pq.top();
+           res.push_back(ele);
+           pq.pop();
+           pq.push(arr[i]);
+       }
+       while(!pq.empty()){
+            int ele=pq.top();
+           res.push_back(ele);
+           pq.pop();
+       }
+       
+       return res;
     }
 };
 
